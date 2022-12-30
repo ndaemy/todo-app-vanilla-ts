27181b2921f09092bb04dd5aed6cf9ef1000ba37
ndaemy <yuyaebean@gmail.com>
2022-12-30 23:38:25 +0900

    ✨ Connect todoStore to localStorage

--- src/store/todoStore.ts
+++ src/store/todoStore.ts
@@ -4,12 +4,25 @@ export type Todo = {
   done: boolean;
 };
 
+const LOCAL_STORAGE_KEY = "todo-app";
+
 class TodoStore {
-  todos: Todo[] = [
-    { id: 1, text: "타입스크립트 배우기", done: true },
-    { id: 2, text: "타입스크립트와 리액트 함께 사용해보기", done: true },
-    { id: 3, text: "프로젝트에 타입스크립트 적용해보기", done: false },
-  ];
+  todos: Todo[] = [];
+  constructor() {
+    const storage = localStorage.getItem(LOCAL_STORAGE_KEY);
+    this.todos = storage
+      ? JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+      : [
+          { id: 1, text: "타입스크립트 배우기", done: true },
+          { id: 2, text: "타입스크립트와 리액트 함께 사용해보기", done: true },
+          { id: 3, text: "프로젝트에 타입스크립트 적용해보기", done: false },
+        ];
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(this.todos));
+  }
+
+  syncStorage() {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(this.todos));
+  }
 
   addTodo = (text: string) => {
     const todo: Todo = {
@@ -20,16 +33,19 @@ class TodoStore {
       done: false,
     };
     this.todos = [...this.todos, todo];
+    this.syncStorage();
   };
 
   updateTodo = (id: number, done: boolean) => {
     this.todos = this.todos.map((todo) =>
       todo.id === id ? { ...todo, done } : todo
     );
+    this.syncStorage();
   };
 
   removeTodo = (id: number) => {
     this.todos = this.todos.filter((todo) => todo.id !== id);
+    this.syncStorage();
   };
 }
 
