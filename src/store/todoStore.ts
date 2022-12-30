627b098bb5d015c1d784c57e7ac45e8038b7a2c6
ndaemy <yuyaebean@gmail.com>
2022-12-30 23:10:07 +0900

    ✨ Create todoStore

---
+++ src/store/todoStore.ts
@@ -0,0 +1,15 @@
+export type Todo = {
+  id: number;
+  text: string;
+  done: boolean;
+};
+
+class TodoStore {
+  readonly todos: Todo[] = [
+    { id: 1, text: "타입스크립트 배우기", done: true },
+    { id: 2, text: "타입스크립트와 리액트 함께 사용해보기", done: true },
+    { id: 3, text: "프로젝트에 타입스크립트 적용해보기", done: false },
+  ];
+}
+
+export const todoStore = new TodoStore();
